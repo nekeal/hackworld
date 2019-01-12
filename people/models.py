2d8ee953ebebe455939855979068af10af1d9b1f
Szymon Cader <szymon.SC.cader@gmail.com>
2019-01-12 14:16:15 +0100

    modified participant model

--- people/models.py
+++ people/models.py
@@ -1,12 +1,26 @@
 from django.db import models
-
+from django.core.validators import ValidationError
 # Create your models here.
 
+def advance_level_validator(value):
+    if value < 1 or value > 10:
+        raise ValidationError("Provide number between 1 and 10")
 
 class City(models.Model):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
+
+
+class Skill(models.Model):
+    name            = models.CharField(max_length=50)
+    advanced_level  = models. SmallIntegerField(validators=[advance_level_validator,])
 
 class Participant(models.Model):
     name        = models.CharField(max_length=50)
     surname     = models.CharField(max_length=50)
     city        = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True)
+    email       = models.EmailField()
+    skills      = models.ManyToManyField(Skill)
