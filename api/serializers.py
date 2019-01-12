7ed7e46c90e79472810c5ab45f01433047555247
Szymon Cader <szymon.SC.cader@gmail.com>
2019-01-12 18:02:27 +0100

    addded cities api

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from people.models import Skill
-
+from people.models import Skill, City
 
 class SkillModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = Skill
+        fields = ('name',)
+
+
+class CityModelSerializer(ModelSerializer):
 
     class Meta:
         model = Skill
--- api/views.py
+++ api/views.py
@@ -2,13 +2,18 @@
 
 from rest_framework.generics import ListAPIView
 from  django_filters.rest_framework import DjangoFilterBackend
-from people.models import Skill
-from .serializers import SkillModelSerializer
-from .filters import SkillFilter
+from people.models import Skill, City
+from .serializers import SkillModelSerializer, CityModelSerializer
+from .filters import SkillFilter, CityFilter
 
 class SkillsListApiView(ListAPIView):
     queryset = Skill.objects.all()
     filter_backends = (DjangoFilterBackend,)
     serializer_class = SkillModelSerializer
     filterset_class = SkillFilter
-    # filter_fields = ('name',)
+
+class CityListApiView(ListAPIView):
+    queryset = City.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    serializer_class = CityModelSerializer
+    filterset_class = CityFilter
