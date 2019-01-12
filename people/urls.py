6abda422d3efc94c7753c5f38002949e2e88633a
Grzegorz Nieużyła <grzegorznieuzyla@gmail.com>
2019-01-12 23:17:42 +0100

    url fixes

--- people/urls.py
+++ people/urls.py
@@ -5,6 +5,6 @@ app_name = 'people'
 urlpatterns = [
     path('register/', ParticipantCreate.as_view(), name='user-register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), sname='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('userprofile/', ProfileView.as_view(), name='profile')
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.http import JsonResponse
 from .models import Team
-
+from people.models import Participant
+from django.core.mail import send_mail
 
 class TeamListView(ListView):
     model = Team
     template_name = 'teams/team-list.html'
+
+
+def notify_teamleader(request):
+    id = request.POST['team-id']
+    participant = Participant.objects.get(request.user)
+    team = Team.objects.filter(id=id)
+    if team.count() == 0:
+        return JsonResponse({'success': False, 'message': f'No team with id={id}'})
+    team = team.first()
+    if team.members.all().count() < 5:
+        return JsonResponse({'success': False, 'message': f'Team is already full'})
+    # if team.members.all()
+
+
+    send_mail(team.teamleader)
+
+
