faadf5553f7d63067709a6bc2ce6595b8d5bceb5
Szymon Cader <szymon.SC.cader@gmail.com>
2019-01-13 05:27:26 +0100

    fix

--- people/urls.py
+++ people/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', ProfileView.as_view(), name='profile'),
-    path('update/', ParticipantUpdateView.as_view(), name='profile'),
+    path('update/', ParticipantUpdateView.as_view(), name='update'),
 ]
