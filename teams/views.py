90ec0211bff9c2d9195bf8944cc3662245d97053
Szymon Cader <szymon.SC.cader@gmail.com>
2019-01-13 13:06:25 +0100

    fix

--- teams/views.py
+++ teams/views.py
@@ -19,7 +19,7 @@ from django.db.models import Q
 
 class AddDeleteCandidate(View):
     def post(self, request):
-        data = json.loads(request.POST)
+        data = json.dumps(request.POST)
         team = Team.objects.get(id=data['team-id'])
         participant = Participant.objects.get(id=data['user-id'])
         if data['accept']:
