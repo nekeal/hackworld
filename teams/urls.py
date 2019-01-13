3b1159c2defb3584ec84b9102e9c4bf52e77ca07
Grzegorz Nieużyła <grzegorznieuzyla@gmail.com>
2019-01-13 12:23:28 +0100

    team update

--- teams/urls.py
+++ teams/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TeamListView, TeamJoinRequestNotifier, TeamCreate
+from .views import TeamListView, TeamJoinRequestNotifier, TeamCreate, TeamUpdateView
 
 urlpatterns = [
     path('list', TeamListView.as_view(), name='team-list'),
     path('join-request/', TeamJoinRequestNotifier.as_view(), name='profile'),
     path('create/', TeamCreate.as_view(), name='team-create'),
+    path('update/<int:pk>', TeamUpdateView.as_view(), name='team-update'),
 ]
