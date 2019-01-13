03c86db60f343f32e6f86439cd3b362340fd9fef
Szymon Cader <szymon.SC.cader@gmail.com>
2019-01-13 13:04:48 +0100

    fix

--- teams/urls.py
+++ teams/urls.py
@@ -3,7 +3,7 @@ from .views import TeamListView, TeamJoinRequestNotifier, TeamCreate, TeamUpdate
 
 urlpatterns = [
     path('list/', TeamListView.as_view(), name='team-list'),
-    path('join-request/', TeamJoinRequestNotifier.as_view(), name='profile'),
+    # path('join-request/', TeamJoinRequestNotifier.as_view(), name='profile'),
     path('create/', TeamCreate.as_view(), name='team-create'),
     path('update/<int:pk>', TeamUpdateView.as_view(), name='team-update'),
     path('join-request/', AddDeleteCandidate.as_view(), name='join-request'),
