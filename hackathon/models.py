4ba174a6797ea37f641f691f7ff64ecb1e3f47a3
Grzegorz Nieużyła <grzegorznieuzyla@gmail.com>
2019-01-13 08:13:33 +0100

    models in hackathon

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime
 from people.models import get_user
+
+
 # Create your models here.
 
 
 class Hackathon(models.Model):
-    name                = models.CharField(max_length=100)
-    official_website    = models.URLField(blank=True)
-    place_url           = models.URLField(blank=True)
-    place               = models.CharField(max_length=100, blank=True)
-    accepted            = models.BooleanField(default=False)
-    description         = models.TextField()
-    proof               = models.TextField(help_text='Put here any information about hackathon and optional links to facebook'
-                                                     'event or official website ')
-    added_by            = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_user)
-    max_size            = models.PositiveIntegerField(default=4)
+    name = models.CharField(max_length=100)
+    official_website = models.URLField(blank=True)
+    place_url = models.URLField(blank=True)
+    place = models.CharField(max_length=100, blank=True)
+    accepted = models.BooleanField(default=False)
+    description = models.TextField()
+    proof = models.TextField(help_text='Put here any information about hackathon and optional links to facebook'
+                                       'event or official website ')
+    added_by = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=get_user)
+    max_size = models.PositiveIntegerField(default=4)
+    facebook_page = models.URLField(blank=True)
+    date = models.DateTimeField(default=datetime.now)
+    image = models.ImageField(blank=True, upload_to='')
 
     def __str__(self):
         return self.name
