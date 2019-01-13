56fc769ebe2504a80214c00d17cd7167adb67c92
Szymon Cader <szymon.SC.cader@gmail.com>
2019-01-13 03:13:21 +0100

    changess

--- people/forms.py
+++ people/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Participant, City, Skill, ParticipantSkill
 from django.forms import ModelForm
+from django.forms.models import inlineformset_factory
 from django.forms.widgets import TextInput
 
 class UserRegisterForm(UserCreationForm):
@@ -10,13 +11,24 @@ class UserRegisterForm(UserCreationForm):
     surname = forms.CharField(max_length=50)
     city = forms.ChoiceField(choices = ((x, x) for x in City.objects.values_list('name', flat=True)), widget=TextInput, required=True)
     skills = forms.ModelMultipleChoiceField(queryset=Skill.objects.all(), required=False)
+    short_description = forms.CharField(max_length=100, required=False)
+    description = forms.TextInput(required=False)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def clean_short_description(self, value):
+        return value or self.instance.short_description
+
+    def clean_description(self, value):
+        return value or self.instance.description
 
 class ParticipantForm(ModelForm):
     # skills = forms.MultipleChoiceField(choices=ParticipantSkill.objects.values_list('id', flat=True))
     class Meta:
         model = Participant
         fields = ['name', 'surname', 'city']
+
+
+ParticipantSkillFormset = inlineformset_factory(Participant, ParticipantSkill, fields=['skill', 'advanced_level'], can_delete=True, extra=3)
--- people/urls.py
+++ people/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ParticipantCreate, LoginView, LogoutView, ProfileView, ParticipantCreateView
+from .views import ParticipantCreate, LoginView, LogoutView, ProfileView, ParticipantCreateView, ParticipantUpdateView
 
 app_name = 'people'
 urlpatterns = [
     path('register/', ParticipantCreate.as_view(), name='user-register'),
-    path('test/', ParticipantCreateView.as_view(), name='user'),
+    path('test/', ParticipantUpdateView.as_view(), name='user'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', ProfileView.as_view(), name='profile'),
