221e8d7bda93f21ee8393e237c062a08e5acf65a
Kamil Krzempek <kamilkrzempek99@gmail.com>
2019-01-13 04:49:54 +0100

    fixes

--- static/js/profile-edit.js
+++ static/js/profile-edit.js
@@ -1,4 +1,5 @@
 let descriptionEdit = false;
+let skillsEdit = false;
 
 $("#description-edit").click(() => {
     if(!descriptionEdit) {
@@ -14,10 +15,71 @@ $("#description-edit").click(() => {
 });
 
 $("#skills-edit").click(() => {
-    $(".user-skills-list").removeClass("block");
-    $(".c-rating button").click((e) => {
-        const value = $(e.currentTarget).text();
-        const ratingElement = $(e.currentTarget).parent();
-        ratingElement.attr("data-rating-value", value);
-    })
+    if(!skillsEdit) {
+        $(".c-rating button").click((e) => {
+            setStarsHandler(e);
+        })
+    }
+    else {
+        $(".c-rating button").off("click");
+    }
+    $(".user-skills").toggleClass("editing");
+    skillsEdit = !skillsEdit;
 });
+
+$("#skills-send").click(() => {
+    const prefix = "participantskill_set-";
+
+    const data = {};
+
+    const total = $(".user-skill").length, initial = total - $(".user-skill-new").length;
+
+    data[prefix + "TOTAL_FORMS"] = total;
+    data[prefix + "INITIAL_FORM"] = initial;
+    data[prefix + "MIN_NUM_FORMS"] = "0";
+    data[prefix + "MAX_NUM_FORMS"] = "1000";
+
+    $(".user-skill").each((index, skill) => {
+        data[prefix + index + "-skill"] = $(skill).find("name").data("id");
+        data[prefix + index + "-advanced_level"] = $(skill).find("c-rating").data("rating-value");
+        data[prefix + index + "-id"] = $(skill).data("id");
+        if($(skill).hasClass("user-skill-remove")) {
+            data[prefix + index + "-DELETE"] = "on";
+        }
+        data[prefix + index + "-participant"] = $(".user-details").data("id");
+    });
+
+
+    console.log(data);
+});
+
+function setStarsHandler(e) {
+    const value = $(e.currentTarget).text();
+    const ratingElement = $(e.currentTarget).parent();
+    ratingElement.attr("data-rating-value", value);
+}
+
+$(".user-skills .add").click((e) => {
+    const newRecord = `
+        <div class="user-skill user-skill-new">
+            <input onkeyup="skillSearch(event)" type="text" list="skills">
+            <datalist id="skills"></datalist>
+            <div class="c-rating c-rating--big" data-rating-value="0">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+            </div>
+        </div>
+    `;
+
+    $(newRecord).insertBefore(e.currentTarget);
+    $(".c-rating button").click((e) => {
+        setStarsHandler(e);
+    })
+})
+
+$(".user-skills .remove").click((e) => {
+    $(e.currentTarget).parent().addClass("user-skills-remove")
+})
--- static/js/skill-select.js
+++ static/js/skill-select.js
@@ -1,16 +1,33 @@
-let prevSkillInput = "";
+function initMultiselect() {
+    $("#skills").multiselect({});
+}
 
-$("#skills").multiselect({});
+let prevSkillInput = "";
 
 $(".search-input").on("keyup", (e) => {
     const skillInput = e.currentTarget.value;
     
-    if(skillInput !== prevSkillInput) {
+    if(skillInput !== prevSkillInput && skillInput !== "") {
         prevSkillInput = skillInput;
         $("#skills").empty();
         $.get("/api/skills", { name: skillInput })
         .done((skills) => {
-            $("#skills").html(skills.map(skill => `<option value="${skill.id}">${skill.name}</option>`).join(""));
+            console.log(skills);
+            $("#skills").html(skills.slice(0, 20).map(skill => `<option id="${skill.id}">${skill.name}</option>`).join(""));
         })
     }
 })
+
+function skillSearch(e) {
+    const skillInput = e.currentTarget.value;
+    
+    if(skillInput !== prevSkillInput && skillInput !== "") {
+        prevSkillInput = skillInput;
+        $("#skills").empty();
+        $.get("/api/skills", { name: skillInput })
+        .done((skills) => {
+            console.log(skills);
+            $("#skills").html(skills.slice(0, 20).map(skill => `<option data-id="${skill.id}">${skill.name}</option>`).join(""));
+        })
+    }
+}
